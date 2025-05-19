4b9c15802c0d3a48e39b77d83aa5b038b80ba5a1
lucasebarreiro <lucasbarreiro1@outlook.com>
2025-05-19 16:25:27 -0300

    proyecto creado

---
+++ AdministradorChatBot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdministradorChatBot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

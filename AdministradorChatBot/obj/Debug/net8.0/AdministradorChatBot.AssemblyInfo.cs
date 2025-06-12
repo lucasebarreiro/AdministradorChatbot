88c5a1ea8ce4a5a41d1ab1dc57b44388f0450160
Lira <ailinvara@gmail.com>
2025-06-12 05:39:40 -0300

    Se arregla el tamaño del footer

--- AdministradorChatBot/obj/Debug/net8.0/AdministradorChatBot.AssemblyInfo.cs
+++ AdministradorChatBot/obj/Debug/net8.0/AdministradorChatBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdministradorChatBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21a86479c5778e744f5734fbda01d58ccb7715e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+029dae379d91d9b630ea84bbfad1af9c67e43d59")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdministradorChatBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdministradorChatBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
